c31cb714bd3530261c371d1b708a8ef41b9b3d61
jocelyn71207 <jocelyn71207@gmail.com>
2026-04-15 11:43:02 +0800

    feat(knowledge): add KnowledgeTablePreview component

---
+++ src/components/Knowledge/KnowledgeTablePreview.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="KnowledgeTablePreview">
+    <table class="custom-table">
+      <thead>
+        <tr>
+          <th v-for="header in data.headers" :key="header">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, ri) in data.rows" :key="ri">
+          <td v-for="(cell, ci) in row" :key="ci">{{ cell }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface TableData {
+  headers: string[]
+  rows: string[][]
+}
+
+defineProps<{ data: TableData }>()
+</script>
