c3f553b2bd4907e3d8235e5e1fadd3b27dc151e9
jocelyn71207 <jocelyn71207@gmail.com>
2026-04-15 11:42:35 +0800

    feat(knowledge): add KnowledgeFlashcardPreview component

---
+++ src/components/Knowledge/KnowledgeFlashcardPreview.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="KnowledgeFlashcardPreview">
+    <div class="flashcard-count">共 {{ cards.length }} 張題卡</div>
+    <div class="flashcard-grid">
+      <div class="flashcard" v-for="(card, i) in cards" :key="i">
+        <div class="flashcard-q-row">
+          <div class="flashcard-q-badge">Q</div>
+          <div class="flashcard-q-text">{{ card.q }}</div>
+        </div>
+        <div class="flashcard-sep"></div>
+        <div class="flashcard-a-row">
+          <div class="flashcard-a-badge">A</div>
+          <div class="flashcard-a-text">{{ card.a }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface FlashcardItem {
+  q: string
+  a: string
+}
+
+defineProps<{ cards: FlashcardItem[] }>()
+</script>
